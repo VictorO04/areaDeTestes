1f9975120eb947f7509dcd50c899f21dcd2c536b
Victor Ferreira <victorcriri313@gmail.com>
2025-05-26 11:50:54 -0300

    update calc

--- calculadoraDePobre.js
+++ calculadoraDePobre.js
@@ -1,32 +1,37 @@
 //comando para instalar a biblioteca readline sync: npm i readline-sync
 
-console.log("Operaçoes matematicas (Nao use acentuacao ou caracteres especiais!!!):");
-console.log("adicao;");
-console.log("subtracao;");
-console.log("multiplicacao;");
-console.log("divisao.");
+console.log("Operaçoes matematicas, escolha a letra da operacao.");
+console.log("a - adicao;");
+console.log("b - subtracao;");
+console.log("c - multiplicacao;");
+console.log("d - divisao.");
 console.log("-----------------------------");
 
 let readline = require("readline-sync");
 
 let operacao = readline.question("Escolha uma operacao matematica: ");
 
+if (operacao !== "a" && operacao !== "b" && operacao !== "c" && operacao !== "d") {
+    console.log("Operação inválida, escolha novamente");
+    operacao = readline.question("Escolha uma operacao matematica: ");
+}
+
 let num1 = readline.questionFloat("escolha o primeiro numero: ");
 let num2 = readline.questionFloat("escolha o segundo numero: ");
 
 switch (operacao) {
-    case "adicao":
+    case "a":
         console.log(`${num1} + ${num2} = ${num1 + num2}`);
         break;
-    case "subtracao":
+    case "b":
         console.log(`${num1} - ${num2} = ${num1 - num2}`);
         break;
-    case "multiplicacao":
+    case "c":
         console.log(`${num1} * ${num2} = ${num1 * num2}`);
         break;
-    case "divisao":
+    case "d":
         console.log(`${num1} / ${num2} = ${num1 / num2}`);
         break;
     default:
-        console.log("ERRO")
+        console.log("ERRO");
 }
